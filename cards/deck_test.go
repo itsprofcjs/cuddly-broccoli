59f3c4c726ced109184266a0acd8d7ad5c791927
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2022-01-06 14:15:18 +0530

    test: added save and read from file test cases

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	deck := newDeck()
@@ -10,4 +13,32 @@ func TestNewDeck(t *testing.T) {
 	if deckLength != 48 {
 		t.Errorf("Expected deck length of 48, but got %v", deckLength)
 	}
+
+	if deck[0] != "Ace Of Spades" {
+		t.Errorf("Expected first card of Ace of Spade but got %v", deck[0])
+	}
+
+	if deck[deckLength-1] != "King Of Clubs" {
+		t.Errorf("Expected last card of King of Clubs but got %v", deck[deckLength-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	fileName := "_decktesting"
+
+	os.Remove(fileName)
+
+	deck := newDeck()
+
+	deck.saveToFile(fileName)
+
+	loadedDeck := newDeckFromFile(fileName)
+
+	deckLength := len(loadedDeck)
+
+	if deckLength != 48 {
+		t.Errorf("Expected deck length of 48 but got %v", deckLength)
+	}
+
+	os.Remove(fileName)
 }
