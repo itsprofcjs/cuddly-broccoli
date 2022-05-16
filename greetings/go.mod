02f12cc39eebbff0559125a802d97332fd60a816
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2022-05-16 19:43:42 +0530

    feat: greetings and hello

---
+++ greetings/go.mod
@@ -0,0 +1,5 @@
+module cjs/greetings
+
+go 1.18
+
+replace cjs/greetings => ../greetings
--- hello/hello.go
+++ hello/hello.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 
 	"rsc.io/quote"
+
+	"cjs/greetings"
 )
 
 func main() {
 	fmt.Println("Hello, CJS!")
 
 	fmt.Println(quote.Go())
+
+	message := greetings.Hello("CJS!")
+
+	fmt.Println(message)
 }
