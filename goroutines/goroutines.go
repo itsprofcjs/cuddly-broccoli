77490d6b766023da7e3eb5f29eeca96a28bb812c
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2022-05-25 15:57:14 +0530

    feat: goroutines exercise

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 	"unicode"
 )
@@ -27,4 +30,75 @@ func main() {
 	time.Sleep(500 * time.Microsecond)
 
 	fmt.Printf("Capitalized: %c\n", capitalized)
+
+	wait := func(ms time.Duration) {
+		time.Sleep(ms * time.Millisecond)
+
+		fmt.Println("sleep duration", ms*time.Millisecond)
+	}
+
+	fmt.Println("Launching goroutines")
+
+	go wait(500)
+	go wait(100)
+	go wait(1000)
+
+	fmt.Println("Launched goroutines")
+
+	time.Sleep(1100 * time.Millisecond)
+
+	fmt.Println("Waited 1100")
+
+	files := []string{"num1.txt", "num2.txt", "num3.txt", "num4.txt", "num5.txt"}
+
+	sum := 0
+
+	for _, file := range files {
+
+		go getSumOfFile(file, &sum)
+
+	}
+
+	time.Sleep(5000 * time.Millisecond)
+
+	fmt.Println("Overall files Sum", sum)
+
+}
+
+func getSumOfFile(filepath string, overallSum *int) (int, error) {
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		fmt.Println("readFile", err)
+		return 0, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+
+	for scanner.Scan() {
+		textNum := scanner.Text()
+		numInLine, err := strconv.Atoi(textNum)
+
+		if err != nil {
+			fmt.Println("numInLine err", err)
+			continue
+		}
+
+		sum += numInLine
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Scanner err", err)
+	}
+
+	fmt.Println("Sum of", filepath, "is", sum)
+
+	*overallSum += sum
+
+	return sum, err
+
 }
