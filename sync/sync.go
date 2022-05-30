f2de2e3ca76512d39cdf386a77449614970cccd2
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2022-05-30 15:00:26 +0530

    feat: sync exercise

--- sync/sync.go
+++ sync/sync.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 func main() {
 
+	c := http.Client{}
+
+	resp, err := c.Get("http://google.com")
+
+	if err != nil {
+		log.Println("Error", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Printf("Error %s", err)
+		return
+	}
+
+	fmt.Printf("Body : %s", body)
+
+	// youtubeMain(&c)
 }
