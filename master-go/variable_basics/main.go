ed387c04cea866bfb08ae2739e0a54bf7082dcfe
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2022-06-07 21:08:07 +0530

    feat: variable basics

--- master-go/variable_basics/main.go
+++ master-go/variable_basics/main.go
@@ -1,9 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	name := "Dan"
 	// unused variables could be used as
 	_ = name
 
-	// fmt.Println("variables", name)
+	fmt.Println("variables", name)
 }
