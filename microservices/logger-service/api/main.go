308746e4fbaf38d9c00c21b7978a2e41de8dd43a
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2022-07-26 21:00:50 +0530

    feat: logger service integration

---
+++ microservices/logger-service/api/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"logger-service/data"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+	Models data.Models
+}
+
+func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+
+	// create a contex in order to disconnect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+
+	defer cancel()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	// start web server
+	app.serve()
+}
+
+func (app *Config) serve() {
+	serv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := serv.ListenAndServe()
+
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Println("Error connecting:", err)
+	}
+
+	return c, nil
+}
